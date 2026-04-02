1b514bc2c4abfb75e27c72c1ac717e3877ab0ccf
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-02 10:36:51 +0200

    feat: add job created event handling and refresh job list

--- jobpulse-front/src/app/app.ts
+++ jobpulse-front/src/app/app.ts
@@ -1,15 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, ViewChild } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { JobListComponent } from './components/job-list.component';
 import { JobFormComponent } from './components/job-form.component';
+import { Job } from './model/job.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, JobListComponent, JobFormComponent],
+  imports: [HttpClientModule, JobListComponent, JobFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('jobpulse-front');
+
+  @ViewChild(JobListComponent) jobListComponent!: JobListComponent;
+
+  /**
+   * Handle job created event and refresh the list
+   */
+  onJobCreated(job: Job): void {
+    console.log('New job created:', job);
+    // Reload the job list to show the new job
+    if (this.jobListComponent) {
+      this.jobListComponent.loadJobs();
+    }
+  }
 }
