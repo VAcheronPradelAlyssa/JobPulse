2dcfaeedb35786440d251726d6d182adb48399fa
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-02 10:35:19 +0200

    feat: implement job creation form

--- jobpulse-front/src/app/app.ts
+++ jobpulse-front/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { JobListComponent } from './components/job-list.component';
+import { JobFormComponent } from './components/job-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, JobListComponent],
+  imports: [RouterOutlet, HttpClientModule, JobListComponent, JobFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
